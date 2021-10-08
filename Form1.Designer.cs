077fff78388035e3458c1e5fa57b45c8039a4fe3
artsonjake2021 <torogitales@gmail.com>
2021-10-08 20:36:02 +0800

    Price Checker System

---
+++ Form1.Designer.cs
@@ -0,0 +1,329 @@
+﻿
+namespace Price_Checker_System
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tb_price = new System.Windows.Forms.TextBox();
+            this.lbl_timer = new System.Windows.Forms.Label();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.lbl_company = new System.Windows.Forms.Label();
+            this.lbl_label = new System.Windows.Forms.Label();
+            this.lbl_description = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tb_barcode = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.panel3);
+            this.panel1.Controls.Add(this.tb_price);
+            this.panel1.Controls.Add(this.lbl_timer);
+            this.panel1.Controls.Add(this.lbl_time);
+            this.panel1.Controls.Add(this.lbl_company);
+            this.panel1.Controls.Add(this.lbl_label);
+            this.panel1.Controls.Add(this.lbl_description);
+            this.panel1.Controls.Add(this.label10);
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.tb_barcode);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(-2, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1452, 850);
+            this.panel1.TabIndex = 0;
+            // 
+            // tb_price
+            // 
+            this.tb_price.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_price.Font = new System.Drawing.Font("Microsoft Sans Serif", 79.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_price.Location = new System.Drawing.Point(507, 228);
+            this.tb_price.Name = "tb_price";
+            this.tb_price.Size = new System.Drawing.Size(933, 151);
+            this.tb_price.TabIndex = 15;
+            this.tb_price.Text = "Scan Barcode";
+            this.tb_price.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lbl_timer
+            // 
+            this.lbl_timer.AutoSize = true;
+            this.lbl_timer.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_timer.Location = new System.Drawing.Point(901, 809);
+            this.lbl_timer.Name = "lbl_timer";
+            this.lbl_timer.Size = new System.Drawing.Size(26, 32);
+            this.lbl_timer.TabIndex = 14;
+            this.lbl_timer.Text = "*";
+            this.lbl_timer.Visible = false;
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.AutoSize = true;
+            this.lbl_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_time.Location = new System.Drawing.Point(557, 809);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(26, 32);
+            this.lbl_time.TabIndex = 13;
+            this.lbl_time.Text = "*";
+            this.lbl_time.Visible = false;
+            // 
+            // lbl_company
+            // 
+            this.lbl_company.AutoSize = true;
+            this.lbl_company.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_company.Location = new System.Drawing.Point(695, 748);
+            this.lbl_company.Name = "lbl_company";
+            this.lbl_company.Size = new System.Drawing.Size(26, 32);
+            this.lbl_company.TabIndex = 12;
+            this.lbl_company.Text = "*";
+            // 
+            // lbl_label
+            // 
+            this.lbl_label.AutoSize = true;
+            this.lbl_label.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_label.Location = new System.Drawing.Point(639, 698);
+            this.lbl_label.Name = "lbl_label";
+            this.lbl_label.Size = new System.Drawing.Size(26, 32);
+            this.lbl_label.TabIndex = 11;
+            this.lbl_label.Text = "*";
+            // 
+            // lbl_description
+            // 
+            this.lbl_description.AutoSize = true;
+            this.lbl_description.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_description.Location = new System.Drawing.Point(557, 638);
+            this.lbl_description.Name = "lbl_description";
+            this.lbl_description.Size = new System.Drawing.Size(27, 32);
+            this.lbl_description.TabIndex = 10;
+            this.lbl_description.Text = "*";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(557, 593);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(271, 32);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Product Description:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(557, 748);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(144, 32);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Company:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(557, 698);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(85, 32);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "label:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Stencil", 22.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(555, 529);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(366, 46);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Product Details";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(557, 445);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(129, 32);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Barcode:";
+            // 
+            // tb_barcode
+            // 
+            this.tb_barcode.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.tb_barcode.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_barcode.Location = new System.Drawing.Point(692, 444);
+            this.tb_barcode.Name = "tb_barcode";
+            this.tb_barcode.Size = new System.Drawing.Size(146, 31);
+            this.tb_barcode.TabIndex = 3;
+            this.tb_barcode.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(710, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(490, 71);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Price Checker";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Stencil", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(575, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(769, 71);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Sunshine Supermarket";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Black;
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(501, 850);
+            this.panel2.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Papyrus", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(3, 557);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(476, 126);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Shopping :)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Papyrus", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(42, 408);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(309, 126);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Happy";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Price_Checker_System.Properties.Resources.spining_sun;
+            this.pictureBox1.Location = new System.Drawing.Point(46, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(396, 345);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(563, 408);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(364, 100);
+            this.panel3.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.ClientSize = new System.Drawing.Size(1450, 850);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox tb_barcode;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lbl_company;
+        private System.Windows.Forms.Label lbl_label;
+        private System.Windows.Forms.Label lbl_description;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lbl_timer;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.TextBox tb_price;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Price_Checker_System
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public MySqlConnection connect;
+        public void con()
+        {
+            string connectionstring;
+            connectionstring = "server=localhost;user id=root;database=db_sunshine_supermarket; password = Info.0515";
+            connect = new MySqlConnection(connectionstring);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            MySqlCommand cmd = new MySqlCommand();
+            string querySelectData = "SELECT * FROM tbl_products where barcode ='" + tb_barcode.Text + "'; ";
+
+            MySqlDataReader reader;
+
+            con();
+            connect.Open();
+            cmd.CommandText = querySelectData;
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = connect;
+            reader = cmd.ExecuteReader();
+
+            if (reader.Read() == true)
+            {
+                
+                tb_price.Text = reader.GetString("price");
+                lbl_description.Text = reader.GetString("description");
+                lbl_label.Text = reader.GetString("label");
+                lbl_company.Text = reader.GetString("company");
+                tb_barcode.SelectAll();
+            }
+
+            else if (reader.Read() == false)
+            {
+                tb_price.Text = "Not Found";
+                lbl_description.Text = "*";
+            }
+            connect.Close();
+        }
+
+        int timeLeft = 3;
+        int timeLeft1 = 30;
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeLeft > 0)
+            {
+                timeLeft = timeLeft - 1;
+                lbl_time.Text = timeLeft + " Seconds";
+                tb_barcode.Select();
+            }
+            else
+            {
+                tb_barcode.SelectAll();
+
+            }
+
+            if (timeLeft1 > 0)
+            {
+                timeLeft1 = timeLeft1 - 1;
+                lbl_timer.Text = timeLeft1 + "";
+            }
+
+            if (lbl_timer.Text == "20")
+            {
+                tb_price.Text = "Scan Barcode";
+                lbl_description.Text = "*";
+                lbl_label.Text = "*";
+                lbl_label.Text = "*";
+
+                tb_barcode.Select();
+            }
+
+            if (timeLeft1 == 0)
+            {
+                timeLeft1 = 30;
+            }
+        }
+    }
+}
